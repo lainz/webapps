1af62b05450ed21cd04339748563fe8c8549bd88
lainz <leandrodiazoe@gmail.com>
2017-05-06 10:01:46 -0300

    fetch cache

--- compras/service-worker.js
+++ compras/service-worker.js
@@ -32,3 +32,12 @@ self.addEventListener('activate', function(e) {
     })
   );
 });
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+});
