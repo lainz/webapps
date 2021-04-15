b26e0f88b89233f4d3aca0c438bf8ffd20f0a92c
Leandro Diaz <lainz@users.noreply.github.com>
2021-04-15 15:34:59 -0300

    FPS

--- sand/app2.js
+++ sand/app2.js
@@ -1,4 +1,22 @@
-"use strict"
+// stats.js - http://github.com/mrdoob/stats.js
+(function (f, e) { "object" === typeof exports && "undefined" !== typeof module ? module.exports = e() : "function" === typeof define && define.amd ? define(e) : f.Stats = e() })(this, function () {
+    var f = function () {
+        function e(a) { c.appendChild(a.dom); return a } function u(a) { for (var d = 0; d < c.children.length; d++)c.children[d].style.display = d === a ? "block" : "none"; l = a } var l = 0, c = document.createElement("div"); c.style.cssText = "position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000"; c.addEventListener("click", function (a) {
+            a.preventDefault();
+            u(++l % c.children.length)
+        }, !1); var k = (performance || Date).now(), g = k, a = 0, r = e(new f.Panel("FPS", "#0ff", "#002")), h = e(new f.Panel("MS", "#0f0", "#020")); if (self.performance && self.performance.memory) var t = e(new f.Panel("MB", "#f08", "#201")); u(0); return {
+            REVISION: 16, dom: c, addPanel: e, showPanel: u, begin: function () { k = (performance || Date).now() }, end: function () {
+                a++; var c = (performance || Date).now(); h.update(c - k, 200); if (c >= g + 1E3 && (r.update(1E3 * a / (c - g), 100), g = c, a = 0, t)) {
+                    var d = performance.memory; t.update(d.usedJSHeapSize /
+                        1048576, d.jsHeapSizeLimit / 1048576)
+                } return c
+            }, update: function () { k = this.end() }, domElement: c, setMode: u
+        }
+    }; f.Panel = function (e, f, l) {
+        var c = Infinity, k = 0, g = Math.round, a = g(window.devicePixelRatio || 1), r = 80 * a, h = 48 * a, t = 3 * a, v = 2 * a, d = 3 * a, m = 15 * a, n = 74 * a, p = 30 * a, q = document.createElement("canvas"); q.width = r; q.height = h; q.style.cssText = "width:80px;height:48px"; var b = q.getContext("2d"); b.font = "bold " + 9 * a + "px Helvetica,Arial,sans-serif"; b.textBaseline = "top"; b.fillStyle = l; b.fillRect(0, 0, r, h); b.fillStyle = f; b.fillText(e, t, v);
+        b.fillRect(d, m, n, p); b.fillStyle = l; b.globalAlpha = .9; b.fillRect(d, m, n, p); return { dom: q, update: function (h, w) { c = Math.min(c, h); k = Math.max(k, h); b.fillStyle = l; b.globalAlpha = 1; b.fillRect(0, 0, r, m); b.fillStyle = f; b.fillText(g(h) + " " + e + " (" + g(c) + "-" + g(k) + ")", t, v); b.drawImage(q, d + a, m, n - a, p, d, m, n - a, p); b.fillRect(d + n - a, m, a, p); b.fillStyle = l; b.globalAlpha = .9; b.fillRect(d + n - a, m, a, g((1 - h / w) * p)) } }
+    }; return f
+});
 
 function requestFullScreen() {
 
@@ -25,13 +43,12 @@ function requestFullScreen() {
 }
 
 class SandBox {
+    stats
     bitmap
     w = 0
     h = 0
     canvas = null
-    contest = null
-    scaleX = 1
-    scaleY = 1
+    context = null
     // size
     size = 1
     speed = 2
@@ -51,12 +68,15 @@ class SandBox {
     constructor() {
         console.log('new SandBox()')
         try {
+            this.stats = new Stats();
+            this.stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+            document.body.appendChild(this.stats.dom);
             // canvas & context
             this.canvas = document.getElementById("canvas")
             this.context = canvas.getContext('2d')
             // canvas size
-            this.canvas.width = screen.width
-            this.canvas.height = screen.height
+            this.canvas.width = Math.round(screen.width / 2)
+            this.canvas.height = Math.round(screen.height / 2)
             // bitmap
             this.w = this.canvas.width
             this.h = this.canvas.height
@@ -100,6 +120,7 @@ class SandBox {
     }
 
     draw() {
+        this.stats.begin();
         // put sand
         if (this.md) {
             for (var y = this.m.y - this.size; y < this.m.y + this.size; y++) {
@@ -138,6 +159,7 @@ class SandBox {
             }
         }
         this.context.putImageData(this.bitmap, 0, 0);
+        this.stats.end();
         window.requestAnimationFrame(this.draw.bind(this))
     }
 
